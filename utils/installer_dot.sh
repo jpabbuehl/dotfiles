cffd49f133b4156788989690026a6425e6727534
Jean-Paul Abbuehl <jp.abbuehl@gmail.com>
2021-12-12 08:22:20 +0100

    revert ssh key

--- utils/installer_dot.sh
+++ utils/installer_dot.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 set -e
 
-[[ -d $HOME/.cfg ]] && echo 'already exist, skipped' || git clone --bare git@github.com:jpabbuehl/dotfiles.git $HOME/.cfg
+[[ -d $HOME/.cfg ]] && echo 'already exist, skipped' || git clone --bare https://github.com/jpabbuehl/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
